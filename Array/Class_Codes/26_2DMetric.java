b3d20bedfdfcdf8d5acb60c0324e94a67e73d802
manish3203 <manishchavan3203@gmail.com>
2023-10-18 01:14:11 +0530

    print 2d metrics

---
+++ Array/Class_Codes/26_2DMetric.java
@@ -0,0 +1,27 @@
+
+/*
+
+   print 2D Metrics row by row
+
+   1  2  3  4
+   5  6  7  8
+   9  10 11 12
+
+*/
+
+class Solution {
+
+	public static void main(String[] args) {
+
+		int arr[][] = new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+		
+		for(int i=0; i<arr.length; i++) {
+
+			for(int j=0; j<arr[i].length; j++) {
+
+				System.out.print(arr[i][j]+"  ");
+			}
+			System.out.println();
+		}
+	}
+}
