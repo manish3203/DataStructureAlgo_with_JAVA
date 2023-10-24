ae670c807b7162bd8166c96f79b76b27cb63de9a
manish3203 <manishchavan3203@gmail.com>
2023-10-24 19:16:51 +0530

    Transpose matrix

---
+++ Array/Class_Codes/34_2DMetric.java
@@ -0,0 +1,34 @@
+
+/*
+
+   34] Given a square matrics of size N*N
+       Convert the matrix to its transpose matrix
+
+*/
+
+class Solution {
+
+	public static void main(String[] args) {
+
+		int arr[][] = new int[][] {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+
+		for(int i=0; i<arr.length; i++) {
+
+			for(int j=i+1; j<arr.length; j++) {
+
+				int temp = arr[i][j];
+				arr[i][j] = arr[j][i];
+				arr[j][i] = temp;
+			}
+		}
+
+		for(int i=0; i<arr.length; i++) {
+
+			for(int j=0; j<arr.length; j++) {
+
+				System.out.print(arr[i][j]+ "  ");
+			}
+			System.out.println();
+		}
+	}
+}
